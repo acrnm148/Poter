12546494b83d45de05d6303d6848348d26297954
acrnm148 <acrnm148>
2021-01-11 13:44:03 +0900

    현재 위치 표시

--- app/src/main/java/com/example/porter/MapActivity.java
+++ app/src/main/java/com/example/porter/MapActivity.java
@@ -1,10 +1,17 @@
 package com.example.porter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -24,6 +31,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.skt.Tmap.TMapData;
+import com.skt.Tmap.TMapGpsManager;
 import com.skt.Tmap.TMapPoint;
 import com.skt.Tmap.TMapPolyLine;
 import com.skt.Tmap.TMapView;
@@ -31,17 +39,18 @@ import com.skt.Tmap.TMapView;
 import java.io.IOException;
 import java.util.List;
 
-public class MapActivity extends AppCompatActivity {
-    TMapView tMapView;
+public class MapActivity extends AppCompatActivity{
+    private boolean m_bTrackingMode = true;
+    private TMapGpsManager tmapgps = null;
+    private TMapView tMapView;
+    private static String AppKey = "l7xx2b1c5cd91b914c2c9c80aab1109ae5d3";
     TMapPoint tMapPointStart = new TMapPoint(37.582191, 127.001915); // 혜화역
     TMapPoint tMapPointEnd = new TMapPoint(37.500628, 127.036392); // 역삼역
-    String AppKey = "l7xx2b1c5cd91b914c2c9c80aab1109ae5d3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-
 
         //상태바 투명 & 아이콘 회색
         Window w = getWindow();
@@ -60,21 +69,36 @@ public class MapActivity extends AppCompatActivity {
         }
 
         //tmap
-        //지도 부분
+        //--지도 부분
         LinearLayout linearLayoutTmap = (LinearLayout)findViewById(R.id.tmap);
         tMapView = new TMapView(this);
         tMapView.setSKTMapApiKey(AppKey);
         linearLayoutTmap.addView( tMapView );
         tMapView.setCenterPoint(127.001691,37.540263,  true);
-        tMapView.setZoomLevel(12);
-        //경로 부분
+
+        //--경로 부분
         TMapPolyLine polyLine = new TMapPolyLine();
         PathAsync pathAsync = new PathAsync();
         pathAsync.execute(polyLine);
+
+        //--현재 위치
+        /* 현재 보는 방향 */
+        tMapView.setCompassMode(true);
+        /* 현위치 아이콘표시 */
+        tMapView.setIconVisibility(true);
+        /* 줌레벨 */
+        tMapView.setZoomLevel(15);
+        tMapView.setMapType(TMapView.MAPTYPE_STANDARD);
+        tMapView.setLanguage(TMapView.LANGUAGE_KOREAN);
+
+        tmapgps = new TMapGpsManager(MapActivity.this);
+        tmapgps.setMinTime(1000);
+        tmapgps.setMinDistance(5);
+        setGps();
     }
 
 
-    //경로 그리는 함수
+    //경로 그리는 클래스
     class PathAsync extends AsyncTask<TMapPolyLine, Void, TMapPolyLine> {
         @Override
         protected TMapPolyLine doInBackground(TMapPolyLine... tMapPolyLines) {
@@ -97,6 +121,40 @@ public class MapActivity extends AppCompatActivity {
             super.onPostExecute(tMapPolyLine);
             tMapView.addTMapPolyLine("Line1", tMapPolyLine);
         }
+    }
+    
+    //현재 위치 함수
+    private final LocationListener mLocationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+
+            if (location != null) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                tMapView.setLocationPoint(longitude, latitude);
+                tMapView.setCenterPoint(longitude, latitude);
+
+            }
+
+        }
+
+        public void onProviderDisabled(String provider) {
+        }
+
+        public void onProviderEnabled(String provider) {
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+    };
+    public void setGps() {
+        final LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, // 등록할 위치제공자(실내에선 NETWORK_PROVIDER 권장)
+                1000, // 통지사이의 최소 시간간격 (miliSecond)
+                1, // 통지사이의 최소 변경거리 (m)
+                mLocationListener);
     }
 
 }
