dcb179d0ac467776539044c21ba5f2d54808da86
acrnm148 <acrnm148>
2021-01-11 11:57:26 +0900

    지도 API 변경 (Google -> TMap)

--- app/src/main/java/com/example/porter/MapActivity.java
+++ app/src/main/java/com/example/porter/MapActivity.java
@@ -3,11 +3,19 @@ package com.example.porter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,14 +23,25 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.skt.Tmap.TMapData;
+import com.skt.Tmap.TMapPoint;
+import com.skt.Tmap.TMapPolyLine;
+import com.skt.Tmap.TMapView;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
-    private GoogleMap mMap;
+import java.io.IOException;
+import java.util.List;
+
+public class MapActivity extends AppCompatActivity {
+    TMapView tMapView;
+    TMapPoint tMapPointStart = new TMapPoint(37.582191, 127.001915); // 혜화역
+    TMapPoint tMapPointEnd = new TMapPoint(37.500628, 127.036392); // 역삼역
+    String AppKey = "l7xx2b1c5cd91b914c2c9c80aab1109ae5d3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
 
         //상태바 투명 & 아이콘 회색
         Window w = getWindow();
@@ -40,24 +59,44 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             getWindow().setStatusBarColor(Color.BLACK);
         }
 
-
-        //GoogleMap 표시
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        //tmap
+        //지도 부분
+        LinearLayout linearLayoutTmap = (LinearLayout)findViewById(R.id.tmap);
+        tMapView = new TMapView(this);
+        tMapView.setSKTMapApiKey(AppKey);
+        linearLayoutTmap.addView( tMapView );
+        tMapView.setCenterPoint(127.001691,37.540263,  true);
+        tMapView.setZoomLevel(12);
+        //경로 부분
+        TMapPolyLine polyLine = new TMapPolyLine();
+        PathAsync pathAsync = new PathAsync();
+        pathAsync.execute(polyLine);
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
-        LatLng SEOUL = new LatLng(37.56, 126.97);
+    //경로 그리는 함수
+    class PathAsync extends AsyncTask<TMapPolyLine, Void, TMapPolyLine> {
+        @Override
+        protected TMapPolyLine doInBackground(TMapPolyLine... tMapPolyLines) {
+            TMapPolyLine tMapPolyLine = tMapPolyLines[0];
+            try {
+                tMapPolyLine = new TMapData().findPathDataWithType(TMapData.TMapPathType.CAR_PATH, tMapPointStart, tMapPointEnd); //출발지, 도착지 설정
+                tMapPolyLine.setLineColor(Color.BLUE);
+                tMapPolyLine.setLineWidth(3);
 
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(SEOUL);
-        markerOptions.title("서울");
-        markerOptions.snippet("한국의 수도");
-        mMap.addMarker(markerOptions);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10));
+
+            }catch(Exception e) {
+                e.printStackTrace();
+                Log.e("error",e.getMessage());
+            }
+            return tMapPolyLine;
+        }
+
+        @Override
+        protected void onPostExecute(TMapPolyLine tMapPolyLine) {
+            super.onPostExecute(tMapPolyLine);
+            tMapView.addTMapPolyLine("Line1", tMapPolyLine);
+        }
     }
+
 }
