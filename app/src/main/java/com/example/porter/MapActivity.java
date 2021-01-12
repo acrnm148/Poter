40277b1524a55a6a4c93e8e856a4593a71bf315f
acrnm148 <acrnm148>
2021-01-12 12:48:26 +0900

    출발 도착 마커 표시 & 현재 위치를 출발지로 설정

--- app/src/main/java/com/example/porter/MapActivity.java
+++ app/src/main/java/com/example/porter/MapActivity.java
@@ -1,51 +1,51 @@
 package com.example.porter;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.common.internal.service.Common;
 import com.skt.Tmap.TMapData;
 import com.skt.Tmap.TMapGpsManager;
+import com.skt.Tmap.TMapPOIItem;
 import com.skt.Tmap.TMapPoint;
 import com.skt.Tmap.TMapPolyLine;
 import com.skt.Tmap.TMapView;
 
-import java.io.IOException;
-import java.util.List;
+import java.util.ArrayList;
 
-public class MapActivity extends AppCompatActivity{
-    private boolean m_bTrackingMode = true;
-    private TMapGpsManager tmapgps = null;
+public class MapActivity extends AppCompatActivity {
+    private TMapGpsManager tMapGps = null;
     private TMapView tMapView;
+    private double lat;
+    private double lon;
+    private double realdistance = 0; //거리값
     private static String AppKey = "l7xx2b1c5cd91b914c2c9c80aab1109ae5d3";
-    TMapPoint tMapPointStart = new TMapPoint(37.582191, 127.001915); // 혜화역
-    TMapPoint tMapPointEnd = new TMapPoint(37.500628, 127.036392); // 역삼역
+    MapPoint mapPoint = new MapPoint();
+
+    private boolean locationState = true;
+    private TMapPoint tMapPointStart;
+    //private TMapPoint tMapPointStart = new TMapPoint(35.17241886016579, 129.1263765979288);//영상물 등급위원회 : 35.17241886016579, 129.1263765979288 //혜화역 : 37.582191, 127.001915
+    private TMapPoint tMapPointEnd = new TMapPoint(35.17127425152002, 129.12722778443444);//영화의 전당: 35.17127425152002, 129.12722778443444 // 역삼역 : 37.500628, 127.036392
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,67 +63,41 @@ public class MapActivity extends AppCompatActivity{
                 view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
                 getWindow().setStatusBarColor(Color.parseColor("#f2f2f2"));
             }
-        }else if (Build.VERSION.SDK_INT >= 21) {
+        } else if (Build.VERSION.SDK_INT >= 21) {
             // 21 버전 이상일 때
             getWindow().setStatusBarColor(Color.BLACK);
         }
 
         //tmap
         //--지도 부분
-        LinearLayout linearLayoutTmap = (LinearLayout)findViewById(R.id.tmap);
+        LinearLayout linearLayoutTmap = (LinearLayout) findViewById(R.id.tmap);
         tMapView = new TMapView(this);
         tMapView.setSKTMapApiKey(AppKey);
-        linearLayoutTmap.addView( tMapView );
-        tMapView.setCenterPoint(127.001691,37.540263,  true);
-
-        //--경로 부분
-        TMapPolyLine polyLine = new TMapPolyLine();
-        PathAsync pathAsync = new PathAsync();
-        pathAsync.execute(polyLine);
+        linearLayoutTmap.addView(tMapView);
+        //tMapView.setCenterPoint(127.001691,37.540263,  true);
 
         //--현재 위치
-        /* 현재 보는 방향 */
-        tMapView.setCompassMode(true);
-        /* 현위치 아이콘표시 */
+        tMapGps = new TMapGpsManager(MapActivity.this);
+        setGps();
+         /* 현위치 아이콘표시 */
         tMapView.setIconVisibility(true);
-        /* 줌레벨 */
+         /* 줌레벨 */
         tMapView.setZoomLevel(15);
         tMapView.setMapType(TMapView.MAPTYPE_STANDARD);
         tMapView.setLanguage(TMapView.LANGUAGE_KOREAN);
+        //tMapView.setCompassMode(true); //현재 보는 방향
+         /*현재 위치 마커 커스텀*/
+        //Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.custom_poi_marker_end);
+        //tMapView.setIcon(bitmap);
 
-        tmapgps = new TMapGpsManager(MapActivity.this);
-        tmapgps.setMinTime(1000);
-        tmapgps.setMinDistance(5);
-        setGps();
+        //--경로 부분
+        drawCashPath(tMapPointStart, tMapPointEnd);
     }
 
 
-    //경로 그리는 클래스
-    class PathAsync extends AsyncTask<TMapPolyLine, Void, TMapPolyLine> {
-        @Override
-        protected TMapPolyLine doInBackground(TMapPolyLine... tMapPolyLines) {
-            TMapPolyLine tMapPolyLine = tMapPolyLines[0];
-            try {
-                tMapPolyLine = new TMapData().findPathDataWithType(TMapData.TMapPathType.CAR_PATH, tMapPointStart, tMapPointEnd); //출발지, 도착지 설정
-                tMapPolyLine.setLineColor(Color.BLUE);
-                tMapPolyLine.setLineWidth(3);
-
-
-            }catch(Exception e) {
-                e.printStackTrace();
-                Log.e("error",e.getMessage());
-            }
-            return tMapPolyLine;
-        }
-
-        @Override
-        protected void onPostExecute(TMapPolyLine tMapPolyLine) {
-            super.onPostExecute(tMapPolyLine);
-            tMapView.addTMapPolyLine("Line1", tMapPolyLine);
-        }
-    }
-    
-    //현재 위치 함수
+    /**
+     * 현재 위치로 표시될 좌표의 위도, 경도를 설정한다.
+     */
     private final LocationListener mLocationListener = new LocationListener() {
         public void onLocationChanged(Location location) {
 
@@ -132,9 +106,13 @@ public class MapActivity extends AppCompatActivity{
                 double longitude = location.getLongitude();
                 tMapView.setLocationPoint(longitude, latitude);
                 tMapView.setCenterPoint(longitude, latitude);
-
+                tMapPointStart = tMapView.getCenterPoint();
+                //--경로 부분 (한번 더 호출)
+                if (locationState == true) { //현재 위치를 출발지로 설정하고 싶으면 locationState가 true여야 함
+                    drawCashPath(tMapPointStart, tMapPointEnd);
+                    locationState = false;
+                }
             }
-
         }
 
         public void onProviderDisabled(String provider) {
@@ -146,6 +124,7 @@ public class MapActivity extends AppCompatActivity{
         public void onStatusChanged(String provider, int status, Bundle extras) {
         }
     };
+
     public void setGps() {
         final LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -155,6 +134,22 @@ public class MapActivity extends AppCompatActivity{
                 1000, // 통지사이의 최소 시간간격 (miliSecond)
                 1, // 통지사이의 최소 변경거리 (m)
                 mLocationListener);
+        tMapPointStart = tMapGps.getLocation();
+    }
+
+    // 경로 그리는 함수
+    public void drawCashPath(TMapPoint tMapPointStart, TMapPoint tMapPointEnd) {
+        TMapData tmapdata = new TMapData();
+        tmapdata.findPathDataWithType(TMapData.TMapPathType.PEDESTRIAN_PATH, tMapPointStart, tMapPointEnd, new TMapData.FindPathDataListenerCallback() {
+            @Override
+            public void onFindPathData(TMapPolyLine polyLine) {
+                polyLine.setLineColor(Color.BLUE);
+                tMapView.addTMapPath(polyLine);
+                realdistance = polyLine.getDistance(); //거리
+                Log.e("(경로그리는함수)", "거리 : " + String.valueOf(realdistance) + "m  " + "출발 : " + tMapPointStart + "  도착 : " + tMapPointEnd);
+                setGps();
+            }
+        });
     }
 
 }
